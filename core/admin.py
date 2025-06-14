4f300f867444156924a8ff705b631f7f001e56ce
Jacob Quaye <kofimave@gmail.com>
2025-06-14 20:00:46 +0000

    added static files

--- core/admin.py
+++ core/admin.py
@@ -4,16 +4,16 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ['first_name', 'last_name', 'is_lecturer', 'is_dro', 'is_fro', 'username']
+    list_display = ['first_name', 'last_name', 'is_lecturer', 'is_dro', 'is_fro', 'is_co','username']
     fieldsets = BaseUserAdmin.fieldsets + (
-        (("UserRoles info"), {"fields": ('is_lecturer', 'is_dro', 'is_fro',)}),        
+        (("UserRoles info"), {"fields": ('is_lecturer', 'is_dro', 'is_fro', 'is_co')}),        
     )
     add_fieldsets = (
         (None, {
             'classes':('wide'),
             'fields':('username', 'password1', 'password2', 
                       'email', 'first_name', 'last_name' ,
-                       'is_lecturer', 'is_dro', 'is_fro'),
+                       'is_lecturer', 'is_dro', 'is_fro', 'is_co'),
         }),
     )
 # Register your models here.
