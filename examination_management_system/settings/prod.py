437ed510b9785706594482235c9f8b869ba45da7
Jacob Quaye <kofimave@gmail.com>
2025-06-30 17:33:10 +0000

    added gmail smtp

--- examination_management_system/settings/prod.py
+++ examination_management_system/settings/prod.py
@@ -7,3 +7,9 @@ ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
