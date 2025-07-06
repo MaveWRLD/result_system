b43c120a593e876377c4e34a8603e19a25b64a05
Jacob Quaye <kofimave@gmail.com>
2025-07-06 06:10:55 +0000

    added cors allowed origins

--- examination_management_system/settings/common.py
+++ examination_management_system/settings/common.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-CORS_ALLOW_ALL_ORIGINS = True
+#CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:8080",
    "http://localhost:3000",
