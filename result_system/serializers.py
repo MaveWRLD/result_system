b599ec3d91af8ad376da5b393ea016988565cb1c
Jacob Quaye <kofimave@gmail.com>
2025-07-13 16:35:36 +0000

    added resul modification features

--- result_system/serializers.py
+++ result_system/serializers.py
@@ -4,15 +4,13 @@ from djoser.serializers import UserCreateSerializer as BaseUserSerializer
 from djoser.serializers import UserSerializer
 from rest_framework import serializers
 
-from .models import (
+from .models import (  # SubmittedResult,; SubmittedResultScore,
     Assessment,
     Course,
     Enrollment,
     Result,
     ResultModificationLog,
     Student,
-    #SubmittedResult,
-    #SubmittedResultScore,
 )
 
 User = get_user_model()
@@ -40,10 +38,18 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class ResultSerializer(serializers.ModelSerializer):
     submitted_at = serializers.DateTimeField(read_only=True)
-    #status = serializers.CharField(read_only=True)
+
+    # status = serializers.CharField(read_only=True)
     class Meta:
         model = Result
-        fields = ["id", "course_id", "created_at", "updated_at", "submitted_at", "status"]
+        fields = [
+            "id",
+            "course_id",
+            "created_at",
+            "updated_at",
+            "submitted_at",
+            "status",
+        ]
 
     def create(self, validated_data):
         course_id = self.context["course_id"]
@@ -51,7 +57,7 @@ class ResultSerializer(serializers.ModelSerializer):
 
 
 class AssessmentSerializer(serializers.ModelSerializer):
-    #student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.none())
+    # student = serializers.PrimaryKeyRelatedField(queryset=Student.objects.none())
 
     class Meta:
         model = Assessment
@@ -67,8 +73,7 @@ class AssessmentSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ("id", "submitted_result_id", "student_id")
 
-
-    #def __init__(self, *args, **kwargs):
+    # def __init__(self, *args, **kwargs):
     #    super().__init__(*args, **kwargs)
     #    result = self.context.get("result")
     #    if result:
@@ -83,62 +88,6 @@ class AssessmentSerializer(serializers.ModelSerializer):
     #            self.fields["student"].queryset = Student.objects.filter(
     #                enrolled_student__course=result.course
     #            ).distinct()
-
-
-#class SubmitResultSerializer(serializers.Serializer):
-#    result_id = serializers.IntegerField(read_only=True)
-#
-#    def save(self, **kwargs):
-#        with transaction.atomic():
-#            result_id = self.context["result_id"]
-#            if Result.objects.filter(id=result_id).exists():
-#                lecturer_id = User.objects.get(id=self.context["lecturer_id"])
-#                course_id = Course.objects.get(id=self.context["course_id"])
-#                submitted_result = SubmittedResult.objects.create(
-#                    lecturer=lecturer_id, course=course_id
-#                )
-#                assessments = Assessment.objects.filter(result_id=result_id)
-#                submitted_result_score = [
-#                    SubmittedResultScore(
-#                        submitted_result=submitted_result,
-#                        student=assessment.student,
-#                        ca_slot1=assessment.ca_slot1,
-#                        ca_slot2=assessment.ca_slot2,
-#                        ca_slot3=assessment.ca_slot3,
-#                        ca_slot4=assessment.ca_slot4,
-#                        exam_mark=assessment.exam_mark,
-#                    )
-#                    for assessment in assessments
-#                ]
-#
-#                SubmittedResultScore.objects.bulk_create(submitted_result_score)
-#                Result.objects.filter(id=result_id).delete()
-#
-
-#class SubmittedResultSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = SubmittedResult
-#        fields = ["id", "course_id", "submitted_at", "result_status", "lecturer_id"]
-
-
-#class SubmittedResultScoreSerializer(serializers.ModelSerializer):
-#    class Meta:
-#        model = SubmittedResultScore
-#        fields = [
-#            "id",
-#            "submitted_result_id",
-#            "student_id",
-#            "ca_slot1",
-#            "ca_slot2",
-#            "ca_slot3",
-#            "ca_slot4",
-#            "exam_mark",
-#        ]
-#        read_only_fields = ("id", "submitted_result_id", "student_id")
-#
-#    def create(self, validated_data):
-#        result_id = self.context["submitted_result_id"]
-#        return Assessment.objects.create(result_id=result_id, **validated_data)
 
 
 class ResultModificationLogSerializer(serializers.ModelSerializer):
@@ -157,8 +106,8 @@ class ResultModificationLogSerializer(serializers.ModelSerializer):
             "modified_at",
         ]
 
-    #def get_student(self, obj):
+    # def get_student(self, obj):
     #    return {
-    #        "id": obj.submitted_result_score.student.id,
-    #        "name": obj.submitted_result_score.student.name,
+    #        "id": obj.assessment.student.id,
+    #        "name": obj.assessment.student.name,
     #    }
