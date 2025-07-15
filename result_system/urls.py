8428c4bc241e2766df9d5f3fa35b578d037273fa
Jacob Quaye <kofimave@gmail.com>
2025-07-15 22:16:50 +0000

    changed url from _ to -

--- result_system/urls.py
+++ result_system/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register('courses', views.CourseViewSet, basename='course')
-router.register('submitted_results', views.SubmittedResultViewSet, basename='submitted_result')
+router.register('submitted-results', views.SubmittedResultViewSet, basename='submitted_result')
 #router.register('results', views.ResultViewSet, basename='result')
 #router.register('assessments', views.AssessmentViewSet)
 
@@ -14,7 +14,7 @@ course_router.register('results', views.ResultViewSet, basename='course-result')
 result_router = NestedDefaultRouter(course_router, 'results', lookup='result')
 result_router.register('assessments', views.AssessmentViewSet, basename='result-assessment')
 
-submitted_result = NestedDefaultRouter(router, 'submitted_results', lookup='submitted_result')
+submitted_result = NestedDefaultRouter(router, 'submitted-results', lookup='submitted_result')
 submitted_result.register('scores', views.SubmittedResultScoreViewSet, basename='submitted_result_score')
 
 urlpatterns = router.urls + course_router.urls + result_router.urls + submitted_result.urls
